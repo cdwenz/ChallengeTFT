4bf80c720b8bc4b5086d9c994abb177b1a3387c6
unknown <cdwenz@gmail.com>
2022-03-25 00:29:44 -0300

    fix validate inputs

--- convert/controller.js
+++ convert/controller.js
@@ -3,14 +3,15 @@ const dateString = require("../dateString/dateString");
 async function pair(value, primary, secondary) {
   let result = await dateString();
   result = await result.data;
+
   if (
+    typeof primary === "string" &&
+    typeof secondary === "string" &&
     result.hasOwnProperty(primary.toUpperCase()) &&
     result.hasOwnProperty(secondary.toUpperCase()) &&
     typeof value === "number"
   ) {
-    let convert =
-      result.rates[secondary.toUpperCase()] /
-      result.rates[primary.toUpperCase()];
+    let convert = result.rates[secondaryValue] / result.rates[primaryValue];
     return convert * value;
   } else {
     return { error: { message: "Invalid currency" } };

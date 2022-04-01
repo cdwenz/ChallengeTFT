3c36a41c38635a9d26294dbfba6cb7b923637b46
unknown <cdwenz@gmail.com>
2022-04-01 20:00:44 -0300

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./app.js");
 require("dotenv").config();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`listening at ${port}`);
